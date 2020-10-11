6d5e0cd1232513321c0e0b29a6b8351cb2a39fb0
amir hossain <a00hossain@gmail>
2020-10-11 21:48:46 +0600

    Added api codes

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,24 @@
 "use strict";
-fetch('https://sms.airproxy.io/api/sms')
-    .then(response => response.json())
-    .then(json => console.log(json))
+
+axios.get('https://sms.airproxy.io/en/api/sms/get_number', {
+   auth: {
+    username: 'AmirSMS',
+    password: 'fn3nc38DAef'
+  } 
+})
+    .then(function (res) {
+        function displayNumber() {
+            document.querySelector(".show-number").innerHTML = res.data[3]['name'];
+        }
+        document.querySelector(".get-number-btn").addEventListener("click", displayNumber);
+    })
+    .catch(function (err) {
+        document.querySelector(".show-number").innerHTML = err;
+    })
+
+
+
+//fetch('https://sms.airproxy.io/api/sms')
+//    .then(response => response.json())
+//    .then(json => console.log(json))
+
